4079ff0dc27e12b171a4222a45cc8c65757f8654
Ceasar Barbosa <ceasarbarbosa@Ceasars-MacBook-Pro.local>
2019-07-22 19:52:48 -0500

    add profile screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_analytics/observer.dart';
 import 'package:flutter/material.dart';
+import 'package:quiz_app/screens/profile.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,6 +11,18 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(),
+      // Firebase Analytics
+      navigatorObservers: [
+        FirebaseAnalyticsObserver(analytics: FirebaseAnalytics())
+      ],
+
+      // Named Routes
+      routes: {
+        // '/': (context) => LoginScreen(),
+        // '/topics': (context) => TopicsScreen(),
+        '/profile': (context) => ProfileScreen(),
+        // '/about': (context) => AboutScreen(),
+      },
     );
   }
 }
---
+++ lib/screens/profile.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:quiz_app/services/auth.dart';
+
+class ProfileScreen extends StatelessWidget {
+  final AuthService auth = AuthService();
+
+  @override
+  Widget build(BuildContext context) {
+    var user = Provider.of<FirebaseUser>(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepOrange,
+        title: Text(user.email),
+      ),
+      body: Center(
+        child: FlatButton(
+          child: Text('logout'),
+          color: Colors.red,
+          onPressed: () async {
+            await auth.signOut();
+            Navigator.of(context)
+                .pushNamedAndRemoveUntil('/', (route) => false);
+          },
+        ),
+      ),
+    );
+  }
+}
